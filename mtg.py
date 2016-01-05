6bd3520130bf9865e1736f443c65266c3d641219
Danilo Raineri <danilo.raineri@tautologico.net>
2016-01-05 13:47:17 +0100

    Bunch of small improvements
    * Always returns an exact match, if found
    * Italic reminder text
    * Mana symbols

--- mtg.py
+++ mtg.py
@@ -20,11 +20,15 @@ def oracle(searchterm):
     data = jsonretrieve(searchterm)
 
     if data:
+        results = data[0:limit]
+        for card in data:
+            if card['name'].lower() == searchterm.lower():
+                results = [card]
         if len(data) > 1:
             answer = u'_Showing only the first results\U00002026_'
         else:
             answer = ''
-        for card in data[0:limit]:
+        for card in results:
             if len(data) > 1:
                 answer += '\n\n'
             answer += '_{name}_, '.format(name = card['name'])
@@ -33,23 +37,45 @@ def oracle(searchterm):
                 answer += u' \U00002014 {subtypes}, '.format(subtypes = ' '.join(card['subtypes']).title())
             else:
                 answer += ', '
-            answer += '*{cost}*\n'.format(cost = card['cost'].encode('utf-8'))
+            answer += encodeCost(card['cost']) +'\n'
+            # answer += '*{cost}*\n'.format(cost = card['cost'].encode('utf-8'))
             if 'creature' in card['types']:
                 answer += u'\U0001F5E1{power}/\U0001F6E1{toughness}\n'.format(power = str(card['power']).encode('utf-8'), 
                     toughness = str(card['toughness']).encode('utf-8'))
             if 'planeswalker' in card['types']:
                 answer += u'\U0001F6E1{loyalty}\n'.format(loyalty = str(card['loyalty']).encode('utf-8'))
-            answer += card['text']
+            oracleText = card['text']
+            oracleText = oracleText.replace('(', '_(')
+            oracleText = oracleText.replace(')', ')_')
+            answer += oracleText
         return answer
     else:
         return ""
+
+def encodeCost(cost):
+    cost = cost.replace('{', '')
+    cost = cost.replace('}', '')
+    cost = cost.replace('W', ':w:')
+    cost = cost.replace('U', ':u:')
+    cost = cost.replace('B', ':n:')
+    cost = cost.replace('G', ':g:')
+    cost = cost.replace('R', ':r:')
+    for colorless in range(15, 0, -1):
+        if cost != cost.replace(str(colorless), ':{}:'.format(str(colorless))):
+            cost = cost.replace(str(colorless), ':{}:'.format(str(colorless)))
+            break
+    return cost
 
 def card(searchterm):
     data = jsonretrieve(searchterm)
 
     if data:
-        card = data[0]
-        return card['editions'][0]['image_url']
+        for card in data:
+            if card['name'].lower() == searchterm.lower():
+                match = card
+            else:
+                match = data[0]
+        return match['editions'][0]['image_url']
     else:
         return ""
 
